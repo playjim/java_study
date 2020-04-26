1a017a0635d4a1492ec5ed2300690371fe60e904
playjim <dim.borisov937@gmail.com>
2020-04-26 22:05:57 +0300

    add project Artifact

---
+++ Artifact/src/main/java/Artifact.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author playjim
+ */
+public class Artifact {
+    int id;
+    String cellar;
+    int century;
+    
+    public Artifact(int id) {
+        this.id = id;
+}
+    public Artifact(int id, String cellar){
+        this.id = id;
+        this.cellar = cellar;
+    }
+    
+    public Artifact(int id, String cellar, int century){
+        this.id = id;
+        this.cellar = cellar;
+        this.century = century;
+    }
+
+    public void printC(Artifact name){
+        System.out.print(name.id +" ");
+        if(name.cellar != null) {
+            System.out.print(name.cellar + " ");
+        }
+        if(name.century != 0) {
+            System.out.print(name.century + " ");
+        }
+    }
+
+    public static void  main(String[] args){
+        Artifact number_one = new Artifact(212121);
+        Artifact number_two = new Artifact(212122,"Ацтеки");
+        Artifact number_three = new Artifact(212123,"КУкухи",21);
+        number_three.printC(number_one);
+    }
+}
