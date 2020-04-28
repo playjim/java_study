17ac22cfc774338ebceb261133b25a9e722cf72a
playjim <dim.borisov937@gmai.com>
2020-04-29 00:49:22 +0300

    Изменения, которые будут включены в коммит: 	новый файл:    .idea/.gitignore 	новый файл:    .idea/.name 	новый файл:    .idea/misc.xml 	изменено:      .idea/modules.xml 	изменено:      .idea/workspace.xml 	новый файл:    Borisov_lab1/.idea/.gitignore 	новый файл:    Borisov_lab1/.idea/misc.xml 	новый файл:    Borisov_lab1/.idea/modules.xml 	новый файл:    Borisov_lab1/.idea/vcs.xml 	новый файл:    Borisov_lab1/Borisov_lab1.iml 	новый файл:    Borisov_lab1/src/lab_1.java 	новый файл:    Borisov_lab1/src/test.java 	переименовано: .idea/java_study.iml -> java_study.iml

---
+++ Borisov_lab1/src/lab_1.java
@@ -0,0 +1,34 @@
+/*
+public class lab_1 {
+    public static void  main(String[] args){
+        Student student = new Student();
+        printtext("Пожалуйста представься"); //student.name
+        printtext("Привет, "+ student.name);
+        printtext("Это программа поможет выучить тебе таблицу умножения, а также пройти экзамен.");
+        printtext("Что ты хочешь учить(введи study) или пройти экзамен(введи exam)?"); //student.mode
+        printtext("Круто! Давай учиться!");
+        printtext("Выбери уровень сложность: 1, 2, 3"); //student.level
+        printtext("От сложности будет зависеть какое количество примеров ты должен будешь решить.");
+
+    }
+    public static void printtext(String text){
+        System.out.println(text);
+    }
+    public static class Student {
+        String name;
+        int level = 1;
+        String mode;
+    }
+    public static int study(Student name){
+        if(student.level == 1){
+            for(int i=0; i<5;i++){
+                Scanner scanner = new Scanner(System.in);
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                int c = scanner.nextInt();
+
+            }
+        }
+    }
+}
+*/
