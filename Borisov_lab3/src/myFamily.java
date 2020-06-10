c201444d7de7ff79d4c35b2709a3a6d74c292290
playjim <dim.borisov937@gmai.com>
2020-06-10 17:58:41 +0300

    3 и 4 лабораторная работа

---
+++ Borisov_lab3/src/myFamily.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class myFamily {
+        public static class Human {
+                String name;
+                int age;
+                String relationship;
+                public Human(String relationship,String name,int age){
+                        this.relationship = relationship;
+                        this.name = name;
+                        this.age = age;
+                }
+                public String toString(){
+                        return relationship+","+name+","+age;
+                }
+                public boolean equals(Human human) {
+                        return this.name ==  human.name;
+                }
+        }
+        public static void main(String[] args) {
+                ArrayList<Human> member = new ArrayList<Human>();
+                member.add(new Human("mother","Alena",48));
+                member.add(new Human("father","Ivan",49));
+                member.add(new Human("son","Dmitry",23));
+                member.add(new Human("daughter","Nastya",24));
+                Iterator<Human> humanIterator = member.iterator();
+                Scanner sc = new Scanner(System.in);
+                String command = "";
+                while(true){
+                        System.out.print(">");
+                        command = sc.nextLine();
+                        if(command.equals("q"))
+                                break;
+                        switch(command)
+                        {
+                                case "1":
+                                        System.out.print("1.Процесс добавление нового объекта в список: \nВведите родство > ");
+                                        String relationship = sc.nextLine();
+                                        System.out.print("Введите имя > ");
+                                        String name = sc.nextLine();
+                                        System.out.print("Введите возраст > ");
+                                        String age =sc.nextLine();
+                                        int ages = Integer.parseInt(age);
+                                        member.add(new Human(relationship,name,ages));
+                                        break;
+                                case "2":
+                                        System.out.println("2.Удаление объекта из списка. \nДля удаления объекта введите его характеристики:");
+                                        System.out.print("Введите родство > ");
+                                        relationship = sc.nextLine();
+                                        System.out.print("Введите имя > ");
+                                        name = sc.nextLine();
+                                        System.out.print("Введите возраст > ");
+                                        age =sc.nextLine();
+                                        ages = Integer.parseInt(age);
+                                        for(int i=0;i<member.size();i++){
+                                                if(member.get(i).name.equals(name) && member.get(i).relationship.equals(relationship) && member.get(i).age == ages){
+                                                        member.remove(i);
+                                                }
+                                        }
+                                        break;
+                                case "3":
+                                        System.out.println("3.Вывод списка:");
+                                        for(int i=0;i<member.size();i++){
+                                                System.out.println(member.get(i));
+                                        }
+                                        break;
+                                case "help":
+                                        help();
+                                default:
+                                        System.out.println("неверная команда");
+                        }// tnd of switch(command)
+                }// tnd ofwhile(true)
+        }// end of main()
+        static void help()
+        {
+                System.out.println("q - Завершение программы");
+                System.out.println("1. Добавить новый объект в список.");
+                System.out.println("2. Удалить объект из списка.");
+                System.out.println("3. Вывести список в консоль.");
+                System.out.println("help - Показать помощь");
+        }
+}
---
+++ Borisov_lab4/src/myFamily.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class myFamily {
+    public static class Human implements Serializable {
+        String name;
+        int age;
+        String relationship;
+        public Human(String relationship,String name,int age){
+            this.relationship = relationship;
+            this.name = name;
+            this.age = age;
+        }
+        public String toString(){
+           return relationship+","+name+","+age;
+        }
+    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ArrayList<Human> member = new ArrayList<>();
+        member.add(new Human("mother","Alena",48));
+        member.add(new Human("father","Ivan",49));
+        member.add(new Human("son","Dmitry",23));
+        member.add(new Human("daughter","Nastya",24));
+        Scanner sc = new Scanner(System.in);
+        String command = "";
+        while(true){
+            System.out.print(">");
+            command = sc.nextLine();
+            if(command.equals("q"))
+                break;
+            switch(command)
+            {
+                case "1":
+                    System.out.print("1.Процесс добавление нового объекта в список: \nВведите родство > ");
+                    String relationship = sc.nextLine();
+                    System.out.print("Введите имя > ");
+                    String name = sc.nextLine();
+                    System.out.print("Введите возраст > ");
+                    String age =sc.nextLine();
+                    int ages = Integer.parseInt(age);
+                    member.add(new Human(relationship,name,ages));
+                    break;
+                case "2":
+                    System.out.println("2.Удаление объекта из списка. \nДля удаления объекта введите его характеристики:");
+                    System.out.print("Введите родство > ");
+                    relationship = sc.nextLine();
+                    System.out.print("Введите имя > ");
+                    name = sc.nextLine();
+                    System.out.print("Введите возраст > ");
+                    age =sc.nextLine();
+                    ages = Integer.parseInt(age);
+                    for(int i=0;i<member.size();i++){
+                        if(member.get(i).name.equals(name) && member.get(i).relationship.equals(relationship) && member.get(i).age == ages){
+                            member.remove(i);
+                        }
+                    }
+                    break;
+                case "3":
+                    System.out.println("3.Вывод списка:");
+                    for(int i=0;i<member.size();i++){
+                        System.out.println(member.get(i));
+                    }
+                    break;
+                case "4":
+                    System.out.println("4.Запись списка в файл out.txt");
+                    FileOutputStream  fos = new FileOutputStream("out.txt");
+                    ObjectOutputStream oos = new ObjectOutputStream(fos);
+                    oos.writeObject(member);
+                    oos.close();
+                    fos.close();
+                    break;
+                case "5":
+                    System.out.println("5.Чтение списка из файла out.txt");
+                    FileInputStream fis = new FileInputStream ("out.txt");
+                    ObjectInputStream ois = new ObjectInputStream (fis);
+                    member = (ArrayList) ois.readObject();
+                    ois.close();
+                    fis.close();
+                    break;
+                case "help":
+                    help();
+                default:
+                    System.out.println("неверная команда, для помощи введите help");
+            }
+        }
+    }
+    static void help()
+    {
+        System.out.println("q - Завершение программы");
+        System.out.println("1. Добавить новый объект в список.");
+        System.out.println("2. Удалить объект из списка.");
+        System.out.println("3. Вывести список в консоль.");
+        System.out.println("4. Записать список в файл out.txt");
+        System.out.println("5. Прочитать список из файла out.txt");
+        System.out.println("help - Показать помощь");
+    }
+}
